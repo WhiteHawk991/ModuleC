da84cc9edea20ff14eb6ced400b0afb7c246085e
WhiteHawk991 <Hawks1299@gmail.com>
2025-01-14 09:31:55 +0300

    Пригодиться

---
+++ Console3.4/Console3.4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class MainClass
+{
+	public static void Main(string[] args)
+	{
+		DaysOfWeek MyFavoriteDay;
+
+		MyFavoriteDay = DaysOfWeek.Friday;
+
+		Console.WriteLine(MyFavoriteDay);
+		Console.ReadKey();
+	}
+}
+
+enum DaysOfWeek : byte
+{
+	Tuesday,
+	Monday,
+	Wednesday,
+	Friday
+}
+
