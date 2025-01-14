15bf211cc7f4e5ea65ef11d3e5ea35e7c48c9410
WhiteHawk991 <Hawks1299@gmail.com>
2025-01-14 10:47:04 +0300

    Save

---
+++ Console3.5/Console3.5/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string str = "Цыган на цыпочках цыпленку цыкнул цыц";
+        Console.Write("Введите строку: ");
+        str = Console.ReadLine();
+        Console.WriteLine("Я ввел: " + str);
+        Console.ReadKey();
+    }
+}
