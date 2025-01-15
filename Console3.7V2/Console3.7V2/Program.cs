bd5cc5160e42285c47a330aac817331c20906673
WhiteHawk991 <Hawks1299@gmail.com>
2025-01-15 09:32:28 +0300

    save

---
+++ Console3.7V2/Console3.7V2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class MainClass
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.Write("Enter your age: ");
+        int age = int.Parse(Console.ReadLine());
+        Console.WriteLine("Your name is {0} and age is {1} ", name, age);
+        Console.ReadKey();
+    }
+
+}
