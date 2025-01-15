1c095ff419b1b0b31b87b7242d5bc72e51169a3f
WhiteHawk991 <Hawks1299@gmail.com>
2025-01-15 09:04:28 +0300

    Коммит урока

---
+++ Console3.7/Console3.7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        int counter = 10;
+        Console.WriteLine("Value: {0} Increment: {1}", counter, counter++);
+
+        int counterOne = 10;
+        Console.WriteLine("Value: {0} Increment: {1}", counterOne, ++counterOne);
+
+        double result = 5.0 / 2.0 * 3.0;
+        Console.WriteLine("Value: {0}", result);
+
+        int MyCase = 4 & 5;
+        Console.WriteLine("Value: {0}", MyCase);
+
+        int b = 10;
+        int k = -5;
+        b = b - k;
+        Console.WriteLine(b);
+
+        double results = 10 % 3;
+        Console.WriteLine(results);
+
+        Console.ReadKey();
+    }
+}
