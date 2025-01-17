d0c70d7c127476295fc108a782ddfdd06d04f75b
WhiteHawk991 <Hawks1299@gmail.com>
2025-01-17 15:10:26 +0300

    4 модуль

---
+++ Console4.0/Console4.0/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        var inv = true;
+        var result = !inv;
+        Console.WriteLine(result);
+        Console.ReadKey();  
+    }
+}
