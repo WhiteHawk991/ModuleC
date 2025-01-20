84cf4ab44bb5fef03fc232b72532a39b68fadf53
WhiteHawk991 <Hawks1299@gmail.com>
2025-01-20 11:16:32 +0300

    Модуль 4.1

---
+++ Console4.1/Console4.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class MainClass
+{
+	public static void Main(string[] args)
+	{
+
+		var a = 6;
+		var b = 7;
+
+		if  (a == b)
+		{
+			Console.WriteLine("Условие истинно");
+		}
+		else
+		{
+			Console.WriteLine("Условие ложно");
+		}
+
+		Console.ReadKey();
+
+	}
+}
