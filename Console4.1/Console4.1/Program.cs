9c47025c3101e38aab602855e7f6586cafbfb346
WhiteHawk991 <Hawks1299@gmail.com>
2025-01-20 11:21:22 +0300

    Условия Если

--- Console4.1/Console4.1/Program.cs
+++ Console4.1/Console4.1/Program.cs
@@ -5,19 +5,23 @@ class MainClass
 	public static void Main(string[] args)
 	{
 
-		var a = 6;
-		var b = 7;
 
-		if  (a == b)
-		{
-			Console.WriteLine("Условие истинно");
-		}
-		else
-		{
-			Console.WriteLine("Условие ложно");
-		}
+        var a = 6;
+        var b = 7;
 
-		Console.ReadKey();
+        if (a == b)
+        {
+            Console.WriteLine("Условие истинно");
+        }
+        else
+        {
+            Console.WriteLine("Условие ложно");
 
-	}
+            if (b < 10)
+                Console.WriteLine("Значение b = {0}", b);
+        }
+
+        Console.ReadKey();
+
+    }
 }
