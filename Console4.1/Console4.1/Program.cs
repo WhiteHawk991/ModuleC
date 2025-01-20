59ad7730451b3f89a08b666f136b84de4dc654f8
WhiteHawk991 <Hawks1299@gmail.com>
2025-01-20 11:24:01 +0300

    Сокращение

--- Console4.1/Console4.1/Program.cs
+++ Console4.1/Console4.1/Program.cs
@@ -4,24 +4,22 @@ class MainClass
 {
 	public static void Main(string[] args)
 	{
+		var a = 6;
+		var b = 7;
 
+		if (a == b && b > 1)
+		{
+			Console.WriteLine("Условие истинно");
+		}
 
-        var a = 6;
-        var b = 7;
-
-        if (a == b)
-        {
-            Console.WriteLine("Условие истинно");
-        }
-        else
-        {
-            Console.WriteLine("Условие ложно");
-
-            if (b < 10)
-                Console.WriteLine("Значение b = {0}", b);
-        }
-
-        Console.ReadKey();
-
-    }
+		else if (b > 10 || b == 7)
+		{
+			Console.WriteLine("Значение b = {0} больше 10 или равно 7", b);
+		}
+		else
+		{
+			Console.WriteLine("Значение b = {0}", b);
+		}
+		Console.ReadKey();
+	}
 }
