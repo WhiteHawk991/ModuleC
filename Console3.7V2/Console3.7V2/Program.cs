fc757878c70fbebc96fadf60764269f87f4e9a5d
WhiteHawk991 <Hawks1299@gmail.com>
2025-01-20 11:04:17 +0300

    Save

--- Console3.7V2/Console3.7V2/Program.cs
+++ Console3.7V2/Console3.7V2/Program.cs
@@ -3,13 +3,12 @@
 class MainClass
 {
     static void Main(string[] args)
-    {
-        Console.Write("Enter your name: ");
-        string name = Console.ReadLine();
-        Console.Write("Enter your age: ");
-        int age = int.Parse(Console.ReadLine());
-        Console.WriteLine("Your name is {0} and age is {1} ", name, age);
-        Console.ReadKey();
-    }
+{
+     Console.Write("Enter your age: ");
+     int age;   
+     bool iscorrect = int.TryParse(Console.ReadLine(), out age);
+     Console.WriteLine("Your age is {0} ", age);
+     Console.ReadKey();
+}
 
 }
---
+++ Console377/Console377/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Console377
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            byte age = checked((byte)int.Parse(Console.ReadLine()));
+            Console.WriteLine("Your name is {0} and age is {1} ", name, age);
+            Console.ReadKey();
+
+        }
+    }
+}
