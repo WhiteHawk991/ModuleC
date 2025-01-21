a174f239d04e3010bbb059cc868fc3d606bf1b58
WhiteHawk991 <Hawks1299@gmail.com>
2025-01-21 09:22:04 +0300

    Обязательно повторить.

--- Console4.3/Console4.3/Program.cs
+++ Console4.3/Console4.3/Program.cs
@@ -2,17 +2,17 @@
 
 class MainClass
 {
-	public static void Main(string[] args)
-	{
-		Console.WriteLine("Введите своё имя");
+    public static void Main(string[] args)
+    {
+        int[,] array = { { 1, 2, 3 }, { 5, 6, 7 }, { 8, 9, 10 }, { 11, 12, 13 } };
 
-		var name = Console.ReadLine();
+        for (int i = 0; i < array.GetUpperBound(1) + 1; i++)
+        {
+            for (int k = 0; k < array.GetUpperBound(0) + 1; k++)
+                Console.Write(array[k,i] + " ");
 
-		for (int i = name.Length-1; i >=0; i--)
-		{
-			Console.Write(name[i] + " ");
-		}
-
-		Console.ReadKey();
-	}
+            Console.WriteLine();
+        }
+        Console.ReadKey();
+    }
 }
