42bd13a2d738185fb1ef7d61440c21590ad47aff
WhiteHawk991 <Hawks1299@gmail.com>
2025-01-21 08:54:10 +0300

    Новый модуль

---
+++ Console4.3/Console4.3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class MainClass
+{
+	public static void Main(string[] args)
+	{
+
+		//Размерность массива 
+		var arr = new int[] { 1, 2, 3, 4 };
+		var l = arr.Length;
+
+		//Разные виды инициализации массива
+		int[] arr1 = new int[4] { 1, 2, 3, 5 };
+		int[] arr2 = new int[] { 1, 2, 3, 5 };
+		int[] arr3 = new[] { 1, 2, 3, 5 };
+		int[] arr4 = { 1, 2, 3, 5 };
+		Console.WriteLine(arr);
+		Console.WriteLine(l);
+		Console.WriteLine(arr1);
+		Console.WriteLine(arr2);
+		Console.WriteLine(arr3);
+		Console.WriteLine(arr4);
+		Console.ReadKey();
+
+	}
+}
