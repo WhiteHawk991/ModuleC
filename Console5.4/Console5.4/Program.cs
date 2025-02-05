19d7c081b4a017ce651e8498b567023fcbe3b6ef
WhiteHawk991 <Hawks1299@gmail.com>
2025-02-05 09:06:58 +0300

    Save

---
+++ Console5.4/Console5.4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace Console5._4
+{
+    class Program
+    {
+        static int num1 = 667;
+
+        static int Sum()
+        {
+            var num2 = 1;
+            return num1 + num2;
+        }
+
+    }
+
+
+}
